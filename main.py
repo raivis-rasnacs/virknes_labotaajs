8b1ab825daf0b223af170a453068023043e8de21
Raivis <71266644+raivis-rasnacs@users.noreply.github.com>
2022-10-12 23:13:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def labo_virkni(virkne, p1, p2):
     if virkne[p1] == virkne[p2]:
         virkne = virkne.replace(virkne[p1], "_", 1)
     print(virkne)
-    labo_virkni(virkne, p1 + 1, p2 + 1)
+    if not p2 + 1 > len(virkne) - 1:
+        labo_virkni(virkne, p1 + 1, p2 + 1)
     
 labo_virkni(virkne, 0, 1)
